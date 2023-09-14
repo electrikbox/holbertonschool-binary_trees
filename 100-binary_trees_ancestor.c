472ee87eb0cf3b99fb56fcaadfb03ddee1767013
electrikbox <electrikbox@icloud.com>
2023-09-14 16:36:15 +0200

    get the ancestor of 2 nodes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,37 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_ancestor -  return a pointer to the LCA node
- * of the two given nodes
- * LCA is lower common ancestor
- * @first: first pointer.
- * @second: second pointer
- * Return: NULL if not ancestor otherwise 0
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: pointer to the lowest common ancestor node of the two given nodes
 */
-
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second)
 {
-	const binary_tree_t *node1 = first;
-	const binary_tree_t *node2 = second;
+	binary_tree_t *pilgrim;
 
 	if (first == NULL || second == NULL)
 		return (NULL);
 
-	while (node1 != node2)
+	pilgrim = (binary_tree_t *)second;
+
+	while (first)
 	{
-		if (node1->parent != NULL)
-		node1 = node1->parent;
-
-		else
-			node1 = second;
-
-		if (node2->parent != NULL)
-		node2 = node2->parent;
-
-		else
-			node2 = first;
-
+		while (second)
+		{
+			if (first == second)
+				return ((binary_tree_t *)first);
+			second = second->parent;
+		}
+		second = pilgrim;
+		first = first->parent;
 	}
-	return ((binary_tree_t *)(node1));
+	return (NULL);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -54,4 +54,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second);
 
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+
 #endif /* _BINARY_TREES_H_ */
